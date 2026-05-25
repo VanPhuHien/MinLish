d035470e6b534496423999873a217df88c6f4325
DuyVo2005 <vlkd1001@gmail.com>
2026-05-25 21:36:59 +0700

    bọc error ở file middle ware  bằng AppError

--- backend/src/middlewares/fileValidate.middleware.js
+++ backend/src/middlewares/fileValidate.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import AppError from "../utils/AppError.js"
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
@@ -7,7 +8,7 @@ const upload = multer({
         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
             fileFilterCallback(null, true);
         } else {
-            fileFilterCallback(new Error('Định dạng file không hợp lệ! Chỉ chấp nhận JPEG hoặc PNG.'), false);
+            fileFilterCallback(new AppError('Định dạng file không hợp lệ! Chỉ chấp nhận JPEG hoặc PNG.', 400), false);
         }
     }
 });
@@ -16,11 +17,11 @@ export const validateAvatar = (req, res, next) => {
     upload.single('avatar')(req, res, (err) => {
         if (err instanceof multer.MulterError) {
             if (err.code === 'LIMIT_FILE_SIZE') {
-                return res.status(400).json({ success: false, message: 'File quá lớn! Tối đa chỉ được 2MB.' });
+                next(new AppError("File quá lớn! Tối đa chỉ được 2MB.", 400));
             }
-            return res.status(400).json({ success: false, message: err.message });
+            next(new AppError(err.message, 400));
         } else if (err) {
-            return res.status(400).json({ success: false, message: err.message });
+            next(new AppError(err.message, 400));
         }
         next(); // Nếu file hợp lệ, cho phép đi tiếp
     });
