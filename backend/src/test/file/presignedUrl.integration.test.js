5ea861159856ea93a63971c1e577ceab0a31dd96
DuyVo2005 <vlkd1001@gmail.com>
2026-06-27 11:11:11 +0700

    chỉnh lại test case sau be thay đổi

--- backend/src/test/file/presignedUrl.integration.test.js
+++ backend/src/test/file/presignedUrl.integration.test.js
@@ -1,7 +1,8 @@
 import User from '../../models/user.model.js';
-import { describe, it, expect, beforeAll } from 'vitest';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import request from 'supertest';
 import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { generateToken } from '../../utils/jwt.js';
 
 const testUserId = new mongoose.Types.ObjectId();
@@ -17,8 +18,12 @@ const adminToken = generateToken(
 const url = '/api/v1/s3/presigned-url';
 
 let app;
+let mongod;
 
 beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+  await mongoose.connect(mongod.getUri());
+
   await User.deleteMany({});
   await User.create({ 
     _id: testUserId, 
@@ -35,6 +40,11 @@ beforeAll(async () => {
   process.env.BUCKET_NAME = process.env.BUCKET_NAME || 'test-bucket';
   process.env.BUCKET_REGION = process.env.BUCKET_REGION || 'ap-southeast-1';
   ({ default: app } = await import('../../app.js'));
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongod.stop();
 });
 
 describe('POST /api/v1/s3/presigned-url', () => {
